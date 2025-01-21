332dd30d9cd530f6f87911604600f44cb4d41e84
Vinuthna <vinuthnanuthakki18@gmail.com>
2025-01-21 17:53:52 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const express = require('express');
 const cors = require("cors");
 require("dotenv").config(); // Load environment variables from .env file
 
